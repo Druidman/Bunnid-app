d32f45dca1546e7dd48bd2410c64904226944785
Mateusz Kowalski <towermanxxl@gmail.com>
2025-10-18 20:08:18 +0200

    added movable windows

---
+++ components/Box.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react"
+import styles from './Box.module.css'
+
+function Box({onClose, boxContent, onMove, startPosition, zIndex}) {
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+    const [isDragging, setIsDragging] = useState(false);
+    const [offset, setOffset] = useState({ x: 0, y: 0 });
+    
+
+    
+
+    const handleMouseDown = (e) => {
+        setIsDragging(true);
+        setOffset({
+            x: e.clientX - position.x,
+            y: e.clientY - position.y,
+        });
+    }
+    const handleMouseMove = (e) => {
+        let x = e.clientX - offset.x
+        let y = e.clientY - offset.y
+        if (isDragging) {
+          setPosition({
+            x: x,
+            y: y
+          });
+          onMove({x: x,y: y})
+        }
+        
+    };
+    const handleMouseUp = () => {
+        setIsDragging(false);
+    };
+    useEffect(()=>{
+        setPosition(startPosition)
+    }, [startPosition])
+    return (
+        
+        <div 
+            className={`${styles.box} box pt-3 has-background-black`}
+            onMouseDown={handleMouseDown}
+            onMouseUp={handleMouseUp}
+            onMouseMove={handleMouseMove}
+            onMouseLeave={handleMouseUp}
+
+            style={{
+                left: position.x,
+                top: position.y,
+                zIndex: zIndex
+
+            }}
+        > 
+            <button onClick={onClose} className="delete is-large"></button>
+            {boxContent}
+        </div>
+        
+    )
+}
+
+export default Box
--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,79 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react'
+import Box from "../components/Box"
+import styles from './App.module.css'
 
-function Counter({max}){
-  const [count, setCount] = useState(0)
-
-  return (
-  <>
-    <button onClick={()=>{setCount(prev => {
-      if (prev + 1 == max){
-        return 0
-      }
-      return prev + 1
-    })}}>
-      Click me
-    </button>
-    <div>{count}</div>
-  </>
-  )
-}
 
 function App() {
- 
+  const [windows, setWindows] = useState([])
+  const [currentlyDraggedWindow, setCurrentlyDraggedWindow] = useState(null)
+  
+  const removeWindow = (id) => {
+    setWindows(prev => prev.filter(item=> item.id !== id))
+  }
+  const handleWindowMove = (newPosition, id) => {
+    setWindows(prev => prev.map((item)=>{
+      if (item.id == id){
+        item.position = newPosition
+      }
+      return item
+    }))
+    if (id != currentlyDraggedWindow){
+      setCurrentlyDraggedWindow(id)
+    }
+    
+  }
+
+  const addWindow = (e) => {
+    setWindows(prev => [...prev, 
+      {
+        id: Date.now(),
+        content: `Move me ${prev.length}`,
+        position: {x: 100, y: 100},
+        zIndex: 0
+      }
+    ])
+  }
+  useEffect(()=>{
+    if (currentlyDraggedWindow == null) return
+
+    setWindows(prev => prev.map((item)=>{
+      if (item.id == currentlyDraggedWindow){
+        item.zIndex = 1
+        
+      }
+      else {
+        item.zIndex = 0
+      }
+      return item
+      
+    }))
+
+  }, [currentlyDraggedWindow])
 
   return (
-    <>
+ 
+    <div className={`${styles.mainPage} is-flex`}>
       <div>
-        Hello From app  
+        <button className="button is-primary" onClick={addWindow}>+</button>
       </div>
-      <Counter max={10}/>
-      <Counter max={2}/>
-      <Counter max={100}/>
-      <Counter max={20}/>
+      <div>
 
-    </>
+        {windows?.map((item)=>(
+          <Box 
+              key={item.id}
+              onClose={()=>removeWindow(item.id)}
+              boxContent={<h1 className="title has-text-warning">{item.content}</h1>}
+              onMove={(newPosition)=>handleWindowMove(newPosition, item.id)}
+              startPosition={item.position}
+              zIndex={item.zIndex}
+          />
+        ))}
+      </div>
+      
+      
+    </div>
+      
+    
   )
 }
 
