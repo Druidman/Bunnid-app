f0f193665d466d19b3e22f1acd41643d39ce0c87
Mateusz Kowalski <towermanxxl@gmail.com>
2025-10-18 16:51:15 +0200

    added max to counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 
-function Counter(){
+function Counter({max}){
   const [count, setCount] = useState(0)
 
   return (
   <>
-    <button onClick={()=>{setCount(prev => prev + 1)}}>
+    <button onClick={()=>{setCount(prev => {
+      if (prev + 1 == max){
+        return 0
+      }
+      return prev + 1
+    })}}>
       Click me
     </button>
     <div>{count}</div>
@@ -22,10 +27,10 @@ function App() {
       <div>
         Hello From app  
       </div>
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
+      <Counter max={10}/>
+      <Counter max={2}/>
+      <Counter max={100}/>
+      <Counter max={20}/>
 
     </>
   )
