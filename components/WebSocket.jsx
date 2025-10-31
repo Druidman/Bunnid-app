fa89c462461b713faba154675b5344e17789cc3c
Mateusz Kowalski <towermanxxl@gmail.com>
2025-10-31 21:33:01 +0100

    working ws server auth with client

--- components/WebSocket.jsx
+++ components/WebSocket.jsx
@@ -1,6 +1,12 @@
 import {useEffect, useRef } from "react"
 import useWebSocket from "react-use-websocket";
 import { BUNNID_API_URL_WS } from '../globals/api'
+import { 
+    REQUEST_TOKEN_MSG, 
+    REQUEST_TOKEN_MSG_REQ_TYPE,
+    ACCESS_DENIED_MSG_INFO_TYPE,
+    ACCESS_GRANTED_MSG_INFO_TYPE
+ } from "../globals/api"
 
 const  WebSocket = ({onNewMessage, messageToSend, token}) => {
     const wsRef = useRef(null);
@@ -15,7 +21,21 @@ const  WebSocket = ({onNewMessage, messageToSend, token}) => {
             console.log('WebSocket connection closed.');
         },
         onMessage: (event) => {
-          
+            
+            let msg = event.data
+
+            msg = JSON.parse(msg)
+
+            if (msg.TYPE == REQUEST_TOKEN_MSG_REQ_TYPE && msg.STATUS){
+                sendMessage(JSON.stringify(REQUEST_TOKEN_MSG(token)))
+            }
+            if (msg.TYPE == ACCESS_DENIED_MSG_INFO_TYPE && msg.STATUS){
+                console.log("Acces denied :(")
+            }
+            if (msg.TYPE == ACCESS_GRANTED_MSG_INFO_TYPE && msg.STATUS){
+                console.log("Acces granted :)")
+            }
+
 
             onNewMessage(event.data)
 
